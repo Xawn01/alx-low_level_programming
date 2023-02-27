936652f8771543e8b88bdbf12db70264f80ba4ba
Shawn Chinguwa <tynoeshawn@gmail.com>
2023-02-27 11:29:55 -0800

    Arrays are not pointers'

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,29 +1,22 @@
-#include "holberton.h"
-#include "stdio.h"
+#include "main.h"
 
 /**
- * print_array - print reverse characters.
- * @a: Array
- * @n: Number of array
- * Return: Always 0.
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < (n - 1); i++)
 	{
-		for (i = 0 ; n > i; i++)
-		{
-			if (i != n - 1)
-			{
-				printf("%d, ", a[i]);
-			}
-			else
-			{
-			       printf("%d", a[i]);
-			}
-		}
+		printf("%d, ", a[i]);
 	}
-	printf("\n");
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
